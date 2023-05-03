fa020f7ee9ed828cbee1d2311a6c6b2b2312b37f
AJCordova <archonist252@gmail.com>
2023-05-03 23:25:47 +0800

    Remove place holder elements.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Tasker from './components/Tasker.vue';
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!--img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
+    </div-->
+    
   </header>
 
   <main>
-    <TheWelcome />
+    <Tasker />
   </main>
 </template>
 
